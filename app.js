7160a215b1e588c9a80c5d6ba61f0fcf37fbfc54
Eero Sova <eero.sova@gmail.com>
2022-05-16 22:44:06 +0300

    Added book REST API

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
+const bookRouter = require('./routes/book');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
+app.use(cors());
+
+
+app.use('/book', bookRouter);
+
+module.exports = app;
